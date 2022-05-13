1de3e81478469fb1f55294a24c6090a13ee4d5a4
hmartinDC <hmartinreina@cifpfbmoll.eu>
2022-05-13 17:55:46 +0200

    Primer Commit. Creación de interface Creación de clase calculadora

--- src/practica5/Calculadora.java
+++ src/practica5/Calculadora.java
@@ -26,5 +26,4 @@ public class Calculadora implements ICalculadora{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    
 }
--- src/practica5/ICalculadora.java
+++ src/practica5/ICalculadora.java
@@ -5,7 +5,6 @@ package practica5;
  * @author HMartin
  */
 public interface ICalculadora {
-    
     public double Suma(double x, double y);
     public double Resta(double x, double y);
     public double Multiplicar(double x, double y);
